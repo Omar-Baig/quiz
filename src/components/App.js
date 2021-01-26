455e3a106fef32b6034031e998bc390e146fd5e2
Omar Baig <obaig992@gmail.com>
2021-01-25 23:43:08 -0500

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import QuestionAPI from './APIs/QuestionAPI';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends React.Component{
+
+    //State
+    state = {question: []}
+
+    //Create async function for when a componenet mounts
+    async componentDidMount(){
+        const response =  await QuestionAPI.get("https://opentdb.com/api.php?amount=10")
+        const data = response.data.results
+        console.log(data)
+    }
+
     render(){
-        return
+        return(
+            <div className="container">
+                <h1 className="section-header text-center">Quiz Website</h1>
+            </div>
+        )
     }
 }
 
